1211ba0e9bbf080707f120c7dc51009a0e12d2d9
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-05-20 15:44:44 +0200

    mapped all items in an array to a dynamic element

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor () {
     super();
     this.state = {
-      string: 'Honk!'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: '1'
+        },
+        {
+          name: 'Dracula',
+          id: '2'
+        },
+        {
+          name: 'Zombie',
+          id: '3'
+        },
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            { this.state.string }
-          </p>
-          <button onClick={() => this.setState({string: 'Bonk!'})}>
-            Change Text
-          </button>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}> { monster.name } </h1>
+          ))
+        }
       </div>
     )
   }
