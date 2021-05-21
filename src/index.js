53c3a761d3cbd128dc78f3c79c4d9dcd947baddb
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-05-21 13:34:21 +0200

    cleaned up canned files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Create a react component
+const App = () => {
+  const buttonText = { text: 'Click me!' };
+  const style = { backgroundColor: 'red', color: 'white' };
+  const labelText = `Enter name: `;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div>
+      <label htmlFor="name" className="label">{ labelText }</label>
+      <input id="name" type="text"/>
+      <button style={style}>{ buttonText.text }</button>
+    </div>
+  )
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
