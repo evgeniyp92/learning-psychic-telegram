85dffb0946bf2d94a78e05208a4c6b724de8837a
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-05-24 11:06:02 +0200

    functional components done

--- proj2-components/src/index.js
+++ proj2-components/src/index.js
@@ -8,6 +8,12 @@ const App = () => {
     return (
         <div className='ui container comments'>
             <ApprovalCard>
+                <div>
+                    <h4>WARNING</h4>
+                    Honkity honk honk
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
                 <CommentDetail
                     name='Sam'
                     timeAgo='Today at 2:54 PM'
@@ -15,18 +21,22 @@ const App = () => {
                     pfp={Faker.image.avatar()}
                 />
             </ApprovalCard>
-            <CommentDetail
-                name='Jon'
-                timeAgo='Today at 9:00 AM'
-                comment={Faker.lorem.sentence()}
-                pfp={Faker.image.avatar()}
-            />
-            <CommentDetail
-                name='Joe'
-                timeAgo='Yesterday at 8:47 PM'
-                comment={Faker.lorem.sentence()}
-                pfp={Faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    name='Jon'
+                    timeAgo='Today at 9:00 AM'
+                    comment={Faker.lorem.sentence()}
+                    pfp={Faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    name='Joe'
+                    timeAgo='Yesterday at 8:47 PM'
+                    comment={Faker.lorem.sentence()}
+                    pfp={Faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
