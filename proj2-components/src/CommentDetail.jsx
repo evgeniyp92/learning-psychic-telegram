1ef9000e549d582b3f7eb1ade8a4c613bb88b2f5
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-05-24 10:33:59 +0200

    reusing components and passing components as children

--- proj2-components/src/CommentDetail.jsx
+++ proj2-components/src/CommentDetail.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const CommentDetail = props => {
-  return (
-    <div className="comment">
-    <a href="/" className="avatar">
-        <img alt="avatar" src={ props.pfp }/>
-    </a>
-    <div className="content">
-      <a href="/" className="author">
-        { props.name }
-      </a>
-      <div className="metadata">
-          <span className="date">{ props.timeAgo }</span>
-      </div>
-      <div className="text">{ props.comment }</div>
-    </div>
-  </div>
-  );
+const CommentDetail = (props) => {
+    return (
+        <div className='comment'>
+            <a href='/' className='avatar'>
+                <img alt='avatar' src={props.pfp} />
+            </a>
+            <div className='content'>
+                <a href='/' className='author'>
+                    {props.name}
+                </a>
+                <div className='metadata'>
+                    <span className='date'>{props.timeAgo}</span>
+                </div>
+                <div className='text'>{props.comment}</div>
+            </div>
+        </div>
+    );
 };
 
-export default CommentDetail
+export default CommentDetail;
--- proj2-components/src/index.js
+++ proj2-components/src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Faker from 'faker';
-import CommentDetail from './CommentDetail'
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <CommentDetail name="Sam" timeAgo="Today at 2:54 PM" comment={Faker.lorem.sentence()} pfp={Faker.image.avatar()}/>
-      <CommentDetail name="Jon" timeAgo="Today at 9:00 AM" comment={Faker.lorem.sentence()} pfp={Faker.image.avatar()}/>
-      <CommentDetail name="Joe" timeAgo="Yesterday at 8:47 PM" comment={Faker.lorem.sentence()} pfp={Faker.image.avatar()}/>
-    </div>
-  );
+    return (
+        <div className='ui container comments'>
+            <ApprovalCard>
+                <CommentDetail
+                    name='Sam'
+                    timeAgo='Today at 2:54 PM'
+                    comment={Faker.lorem.sentence()}
+                    pfp={Faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <CommentDetail
+                name='Jon'
+                timeAgo='Today at 9:00 AM'
+                comment={Faker.lorem.sentence()}
+                pfp={Faker.image.avatar()}
+            />
+            <CommentDetail
+                name='Joe'
+                timeAgo='Yesterday at 8:47 PM'
+                comment={Faker.lorem.sentence()}
+                pfp={Faker.image.avatar()}
+            />
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
