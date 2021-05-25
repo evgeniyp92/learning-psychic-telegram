f28d758f27f93c9891d1d808e20a9ae5c42b8eda
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-05-25 16:28:56 +0200

    end of day commit

--- proj4-inputs-events-lists/src/components/SearchBar.jsx
+++ proj4-inputs-events-lists/src/components/SearchBar.jsx
@@ -3,20 +3,15 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     state = { term: '' };
 
-    // Prevent the page from refreshing
-    // Fixed by using an arrow function
-    // To get the parent this
     onFormSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state.term);
+        console.log(this.state);
     };
 
     render() {
         return (
             <div className='ui segment'>
                 <form onSubmit={this.onFormSubmit} className='ui form'>
-                    {/* Alternate way of handling broken thises below */}
-                    {/* <form onSubmit={(event) => this.onFormSubmit()}></form> */}
                     <div className='field'>
                         <label>Image Search</label>
                         <input
