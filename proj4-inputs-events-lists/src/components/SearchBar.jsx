a3d167a991126dec449e3b0eb7c6049791eca57d
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-05-25 16:19:24 +0200

    added a submit handler and introduced a context issue

--- proj4-inputs-events-lists/src/components/SearchBar.jsx
+++ proj4-inputs-events-lists/src/components/SearchBar.jsx
@@ -3,10 +3,19 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     state = { term: '' };
 
+    // Prevent the page from refreshing
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // this will break because the this keyword is goofy
+        // within the context of this function, 'this' is undefined
+        console.log(this.state.term);
+    }
+
     render() {
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form onSubmit={this.onFormSubmit} className='ui form'>
                     <div className='field'>
                         <label>Image Search</label>
                         <input
