70e52c258003a9b665accd67b4852a4828820641
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-05-25 10:01:54 +0200

    added componentDidMount()

--- proj3-class-components/src/index.js
+++ proj3-class-components/src/index.js
@@ -6,7 +6,11 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = { lat: null, errorMessage: `` };
+    }
 
+    // predefined function name for javascript code to be executed when the component renders
+    componentDidMount() {
+        console.log(`App rendered`);
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 // We called setState!
@@ -21,19 +25,32 @@ class App extends React.Component {
         );
     }
 
+    // predefined function name for javascript code to be executed
+    // when a state change is detected and a component needs to be rendered
+    componentDidUpdate() {
+        console.log(`App updated/rerendered`);
+    }
+
     // We must define render()
     render() {
         // Outlining conditional rendering in a document
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
-
         if (!this.state.errorMessage && this.state.lat) {
             return <div>Latitude {this.state.lat}</div>;
         }
-
         return <div>Loading...</div>;
     }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+// OUTLINE OF THE COMPONENT LIFECYCLE
+// constructor() - good place to do one-time setup, but try to avoid data loading here, not best practice
+// render() - avoid doing anything besides returning jsx
+// componentDidMount() - good place to load data, best practice
+// componentDidUpdate() - good place to load data when state/props change
+// componentWillUnmount() - good place to do housekeeping after your component is removed
+// ^^^ this isnt exhaustive
+// shouldComponentUpdate(), getDerivedStateFromProps(), getSnapshotBeforeUpdate() also exist
