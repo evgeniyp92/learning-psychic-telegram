f9a6317937ca922e497a9c20069042b6dc7c1b28
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-05-26 14:00:44 +0200

    add axios get request

--- proj4-inputs-events-lists/src/components/App.jsx
+++ proj4-inputs-events-lists/src/components/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import axios from 'axios';
+
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(searchTerm) {
-        console.log(searchTerm);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: searchTerm,
+            },
+            headers: {
+                Authorization:
+                    'Client-ID KkeTma_ruzAufux-Xueh0higHNIiJ3re6H0jxQRmVck',
+            },
+        });
     }
 
     render() {
