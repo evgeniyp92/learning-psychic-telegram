520bebaa422912d75aa55149b68eaf8e24bc70bc
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-06-21 14:18:25 +0200

    search component rewritten and done

--- proj6-hooks/src/components/Search.jsx
+++ proj6-hooks/src/components/Search.jsx
@@ -3,28 +3,38 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState(`programming`);
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
 
-    useEffect(() => {
-        // useEffect cannot be async
-        // WORKAROUND 1
-        //// const searchArticles = async () => {
-        ////     await axios.get('blablabla');
-        //// };
-        //// searchArticles();
-        ////
-        ////
-        // WORKAROUND 2 - IIFE
-        //// (async () => {
-        ////     await axios.get('blablabla');
-        //// })();
-        ////
-        ////
-        // WORKAROUND 3 -   JUST USE PROMISES BRO
-        //// axios.get('blablabla').then((response) => {
-        ////     // console.log(response.data)
-        //// });
+    // useEffect cannot be async
+    // WORKAROUND 1
+    //// const searchArticles = async () => {
+    ////     await axios.get('blablabla');
+    //// };
+    //// searchArticles();
+    ////
+    ////
+    // WORKAROUND 2 - IIFE
+    //// (async () => {
+    ////     await axios.get('blablabla');
+    //// })();
+    ////
+    ////
+    // WORKAROUND 3 -   JUST USE PROMISES BRO
+    //// axios.get('blablabla').then((response) => {
+    ////     // console.log(response.data)
+    //// });
 
+    useEffect(() => {
+        const timerID = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+        return () => {
+            clearTimeout(timerID);
+        };
+    }, [term]);
+
+    useEffect(() => {
         const search = async () => {
             const { data } = await axios.get(
                 'https://en.wikipedia.org/w/api.php',
@@ -34,28 +44,15 @@ const Search = () => {
                         list: 'search',
                         origin: '*',
                         format: 'json',
-                        srsearch: term,
+                        srsearch: debouncedTerm,
                     },
                 },
             );
             setResults(data.query.search);
         };
 
-        if (term && !results.length) {
-            search();
-        } else {
-            const timeoutID = setTimeout(() => {
-                if (term) {
-                    search();
-                }
-            }, 500);
-
-            // the things outlined below return at the end of useEffect will run right before useEffect triggers again
-            return () => {
-                clearTimeout(timeoutID);
-            };
-        }
-    }, [results.length, term]);
+        search();
+    }, [debouncedTerm]);
 
     const renderedResults = results.map((result) => {
         return (
@@ -99,3 +96,6 @@ const Search = () => {
 };
 
 export default Search;
+
+// https://www.amazon.com/dp/B087GCFMS1?tag=theverge02-20&ascsubtag=[]vg[p]22302533[t]w[r]theverge.com[d]D
+// https://www.amazon.com/dp/B07X5MWMDG?tag=theverge02-20&ascsubtag=[]vg[p]22302533[t]w[r]theverge.com[d]D
