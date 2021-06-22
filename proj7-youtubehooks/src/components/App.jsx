a8af5817eea7db7f7f7bed14a51658f06edf63d0
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-06-22 14:27:19 +0200

    app refactor done

--- proj7-youtubehooks/src/components/App.jsx
+++ proj7-youtubehooks/src/components/App.jsx
@@ -1,58 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+import youtube from "../apis/youtube";
 
-import SearchBar from './SearchBar';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-import youtube from '../apis/youtube';
+  useEffect(() => {
+    onTermSubmit("react.js");
+  }, []);
 
-export class App extends Component {
-    state = {
-        videos: [],
-        selectedVideo: null,
-    };
+  const onTermSubmit = async term => {
+    const r = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    setVideos(r.data.items);
+    setSelectedVideo(r.data.items[0]);
+  };
 
-    componentDidMount() {
-        this.onTermSubmit('react.js');
-    }
+  return (
+    <div className='ui container'>
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <div className='ui grid'>
+        <div className='ui row'>
+          <div className='eleven wide column'>
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className='five wide column'>
+            <VideoList
+              onVideoSelect={v => setSelectedVideo(v)}
+              videos={videos}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    onTermSubmit = async (term) => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term,
-            },
-        });
-        this.setState({
-            videos: response.data.items,
-            selectedVideo: response.data.items[0],
-        });
-    };
-
-    onVideoSelect = (video) => {
-        console.log('From the app!', video);
-        this.setState({ selectedVideo: video });
-    };
-
-    render() {
-        return (
-            <div className='ui container'>
-                <SearchBar onFormSubmit={this.onTermSubmit} />
-                <div className='ui grid'>
-                    <div className='ui row'>
-                        <div className='eleven wide column'>
-                            <VideoDetail video={this.state.selectedVideo} />
-                        </div>
-                        <div className='five wide column'>
-                            <VideoList
-                                onVideoSelect={this.onVideoSelect}
-                                videos={this.state.videos}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+// ------------------------------------------------------------------------
 
 export default App;
