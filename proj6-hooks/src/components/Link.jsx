94538cd0f0ab595980de48f7b4bcde85b8e71351
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-06-22 13:31:19 +0200

    nav done

--- proj6-hooks/src/components/Link.jsx
+++ proj6-hooks/src/components/Link.jsx
@@ -2,6 +2,12 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
   const onClick = e => {
+    // if the user is holding down meta or ctrl,
+    // let the event execute
+    if (e.metaKey || e.ctrlKey) {
+      return;
+    }
+
     // prevent page reload
     e.preventDefault();
     // change the browser url
