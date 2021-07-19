f84bdb78148523a1f66e67c4b266fe095f09a528
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-07-19 12:43:42 +0200

    added UserHeader component, connected lookup

--- proj9-redux-thunk/src/reducers/index.js
+++ proj9-redux-thunk/src/reducers/index.js
@@ -19,8 +19,10 @@
 import { combineReducers } from 'redux';
 
 import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
+  users: usersReducer,
   posts: postsReducer,
   isTrue: () => true,
 });
