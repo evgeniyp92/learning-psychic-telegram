26cb63432e981ed341f1395e0ce2ce5fafe2ebf5
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-07-19 11:03:56 +0200

    added UserHeader, wired up redux

--- proj9-redux-thunk/src/actions/index.js
+++ proj9-redux-thunk/src/actions/index.js
@@ -43,3 +43,9 @@ export const fetchPosts = () => async (dispatch, getState) => {
 
   dispatch({ type: `FETCH_POSTS`, payload: response.data });
 };
+
+export const fetchUser = id => async (dispatch, getState) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
--- proj9-redux-thunk/src/components/PostList.jsx
+++ proj9-redux-thunk/src/components/PostList.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 
 import { fetchPosts } from '../actions';
 
+import UserHeader from './UserHeader';
+
 // generally, components should be responsible for fetching
 // data they need by calling an action creator
 
@@ -28,6 +30,7 @@ export class PostList extends Component {
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId} />
           </div>
         </div>
       );
