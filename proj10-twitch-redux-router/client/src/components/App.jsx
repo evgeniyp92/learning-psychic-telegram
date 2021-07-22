a38f8c021b71089c803bfc4ec9d848192987cd4c
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-07-22 09:25:54 +0200

    minor additional scaffolding

--- proj10-twitch-redux-router/client/src/components/App.jsx
+++ proj10-twitch-redux-router/client/src/components/App.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const PageOne = () => {
-  return (
-    <div>
-      Page One<Link to='/pagetwo'>Go to Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      Page Two<Link to='/'>Go to Page One</Link>
-      <button>Click Me</button>
-    </div>
-  );
-};
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
+      <h1>Strimmer.io</h1>
       <BrowserRouter>
         <div>
-          <Route path='/' exact component={PageOne} />
-          <Route path='/pagetwo' component={PageTwo} />
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' exact component={StreamCreate} />
+          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/delete' exact component={StreamDelete} />
+          <Route path='/streams/show' exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
