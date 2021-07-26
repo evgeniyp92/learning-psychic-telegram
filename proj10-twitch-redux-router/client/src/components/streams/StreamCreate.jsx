aebcf03571a8ec960b70ecccd565952e19b236fc
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-07-26 16:20:15 +0200

    end of day commit 7/26

--- proj10-twitch-redux-router/client/src/components/streams/StreamCreate.jsx
+++ proj10-twitch-redux-router/client/src/components/streams/StreamCreate.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+// here, Field is a component and reduxForm is a function
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+export class StreamCreate extends Component {
+  renderInput({ input }) {
+    return (
+      <input
+        {...input}
+        // onChange={formProps.input.onChange}
+        // value={formProps.input.value}
+      />
+    );
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field name='title' component={this.renderInput} />
+        <Field name='description' component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
