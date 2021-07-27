4fdc1649429e71914f8f221ef6aaae7a975e9af8
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-07-27 10:46:15 +0200

    handling form submission with redux form

--- proj10-twitch-redux-router/client/src/components/streams/StreamCreate.jsx
+++ proj10-twitch-redux-router/client/src/components/streams/StreamCreate.jsx
@@ -3,21 +3,35 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 export class StreamCreate extends Component {
-  renderInput({ input }) {
+  renderInput({ input, label }) {
     return (
-      <input
-        {...input}
-        // onChange={formProps.input.onChange}
-        // value={formProps.input.value}
-      />
+      <div className='field'>
+        <label>{label}</label>
+        <input
+          {...input}
+          // onChange={formProps.input.onChange}
+          // value={formProps.input.value}
+        />
+      </div>
     );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
   }
 
   render() {
     return (
-      <form>
-        <Field name='title' component={this.renderInput} />
-        <Field name='description' component={this.renderInput} />
+      <form
+        className='ui form'
+        onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field name='title' component={this.renderInput} label='Title' />
+        <Field
+          name='description'
+          component={this.renderInput}
+          label='Description'
+        />
+        <button className='ui button primary'>Submit</button>
       </form>
     );
   }
