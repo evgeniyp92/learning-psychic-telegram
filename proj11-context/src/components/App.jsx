ef9dba6ea94c51eb483f4bc7293fb6e30a903734
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-09-29 14:32:17 +0200

    using multiple context providers

--- proj11-context/src/components/App.jsx
+++ proj11-context/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 import UserCreate from './UserCreate';
 
@@ -42,9 +43,11 @@ class App extends Component {
             onClick={() => this.onLanguageChange('dutch')}></i>
         </div>
         {/* Providing context and data to the UserCreate component, and the LanguageContext */}
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value='red'>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
--- proj11-context/src/components/Button.jsx
+++ proj11-context/src/components/Button.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 export class Button extends Component {
   // This is how you wire up context
@@ -7,16 +8,25 @@ export class Button extends Component {
   // static contextType = LanguageContext;
   // once context is set up it can be accessed from this.context
   // when using a consumer you do not need to set up contextType
+  // using this.context is not great because you cannot access multiple context objects
 
-  render() {
-    // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+  renderButton(color) {
     return (
-      <button className='ui primary button'>
+      <button className={`ui button ${color}`}>
         {/* 'Consuming' data from the language context */}
         <LanguageContext.Consumer>
           {value => (value === 'english' ? 'Submit' : 'Voorleggen')}
         </LanguageContext.Consumer>
       </button>
+    );
+  }
+
+  render() {
+    // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
     );
   }
 }
