ab42502549e670726547167a68158292dafec7cf
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-09-29 13:36:37 +0200

    setting up context and getting a piece of context
    utilising this.context

--- proj11-context/src/components/Button.jsx
+++ proj11-context/src/components/Button.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 export class Button extends Component {
+  // This is how you wire up context
+  // contextType is a very special name in React
+  static contextType = LanguageContext;
+  // once context is set up it can be accessed from this.context
+
   render() {
+    console.log(this.context);
     return <button className='ui primary button'>Submit</button>;
   }
 }
+
+// Alternatively, you can set up context by writing the below:
+// Button.contextType = LanguageContext
 
 export default Button;
