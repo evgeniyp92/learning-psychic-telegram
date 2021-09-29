235308b572d87da090d6a1e41d2df92c48e95c2d
Evgeniy Pimenov <1@evgeniypimenov.com>
2021-09-29 14:21:22 +0200

    accesing data with consumers

--- proj11-context/src/components/Button.jsx
+++ proj11-context/src/components/Button.jsx
@@ -4,12 +4,20 @@ import LanguageContext from '../contexts/LanguageContext';
 export class Button extends Component {
   // This is how you wire up context
   // contextType is a very special name in React
-  static contextType = LanguageContext;
+  // static contextType = LanguageContext;
   // once context is set up it can be accessed from this.context
+  // when using a consumer you do not need to set up contextType
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
-    return <button className='ui primary button'>{text}</button>;
+    // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return (
+      <button className='ui primary button'>
+        {/* 'Consuming' data from the language context */}
+        <LanguageContext.Consumer>
+          {value => (value === 'english' ? 'Submit' : 'Voorleggen')}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
